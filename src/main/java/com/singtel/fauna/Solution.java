c770c7b39e720c3b40bf306ccb90defed1babfa9
arpitkhandelwal1984 <arpit.khandelwal.1984@gmail.com>
2018-06-11 17:22:34 +0530

    Section E. Counting animals

--- src/main/java/com/singtel/fauna/Solution.java
+++ src/main/java/com/singtel/fauna/Solution.java
@@ -1,5 +1,9 @@
 package com.singtel.fauna;
 
+import com.singtel.fauna.behaviour.ICanFly;
+import com.singtel.fauna.behaviour.ICanSing;
+import com.singtel.fauna.behaviour.ICanSwim;
+import com.singtel.fauna.behaviour.ICanWalk;
 import com.singtel.fauna.bird.Bird;
 import com.singtel.fauna.bird.Chicken;
 import com.singtel.fauna.bird.Duck;
@@ -29,8 +33,40 @@ public class Solution {
         Bird duck = new Duck();
         Bird chicken = new Chicken(Chicken.ChickenType.FEMALE);
         Bird rooster = new Chicken(Chicken.ChickenType.MALE);
+        Bird parrot = new Parrot();
+        ((Parrot)parrot).setNeighbour(cat);
 
         Animal dolphin = new Dolphin();
 
+        Animal[] animals = new Animal[]{
+                cat,dog,clownFish,shark,duck,chicken,rooster,parrot,dolphin
+        };
+        analyse(animals);
+    }
+
+    private static void analyse(Animal[] animals){
+        int canFly = 0;
+        int canWalk = 0;
+        int canSing = 0;
+        int canSwim = 0;
+        for (Animal animal:animals) {
+            if(animal instanceof ICanFly){
+                canFly++;
+            }
+            if(animal instanceof ICanWalk){
+                canWalk++;
+            }
+            if(animal instanceof ICanSing){
+                canSing++;
+            }
+            if(animal instanceof ICanSwim){
+                canSwim++;
+            }
+        }
+
+        System.out.println("Number of Animals that can Fly = " + canFly + "\n " +
+                           "Number of Animals that can Walk = " + canWalk + "\n " +
+                           "Number of Animals that can Sing = " + canSing + "\n " +
+                           "Number of Animals that can Swim = " + canSwim);
     }
 }
--- src/main/java/com/singtel/fauna/mammals/Mammal.java
+++ src/main/java/com/singtel/fauna/mammals/Mammal.java
@@ -2,9 +2,10 @@ package com.singtel.fauna.mammals;
 
 import com.singtel.fauna.Animal;
 import com.singtel.fauna.behaviour.ICanSay;
+import com.singtel.fauna.behaviour.ICanWalk;
 
 /**
  * Created by Arpit Khandelwal.
  */
-public abstract class Mammal extends Animal implements ICanSay {
+public abstract class Mammal extends Animal implements ICanSay, ICanWalk {
 }
